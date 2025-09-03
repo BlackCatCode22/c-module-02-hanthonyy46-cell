4b53f11bbcdc8b83223bdb502b0b665637d6633e
hanthonyy46-cell <hanthonyy46@gmail.com>
2025-09-03 11:59:49 -0700

    Add files via upload

---
+++ Funwfuction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+// getAnIntFromTheUser
+
+int getAnIntFromTheUser(string username) {
+    int anIntfromtheuser;
+
+    cout << "\n hello, " + username + ", please enter a whole number ";
+    cin >> anIntfromtheuser;
+
+    return anIntfromtheuser;
+
+int main() {
+    cout << "\n Morning brain \n";
+    string username;
+    int anIntfromtheuser;
+    cout << "\n Enter your username: ";
+    cin >> username;
+    cout << "\n udername is: " << username << "\n";
+
+    aRandomNumber = getAnIntFromTheUser(username);
+    COUT << "\N\N" + username + " is a random number \n";
+
+return 0;
+
+)
